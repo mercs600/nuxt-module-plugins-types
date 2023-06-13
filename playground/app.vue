915d54d5b9c3483ef7c2a7d9f0d3ff1c7a475a1a
Adam <a.marcinkowski@macopedia.com>
2023-06-13 13:50:04 +0200

    init

---
+++ playground/app.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    Nuxt module playground!
+  </div>
+</template>
+
+<script setup lang="ts">
+import { useNuxtApp } from '#app';
+
+const nuxtApp = useNuxtApp()
+nuxtApp.$myPlugin.hello
+</script>
---
+++ src/runtime/plugin.ts
@@ -0,0 +1,12 @@
+import { defineNuxtPlugin } from '#app'
+
+export default defineNuxtPlugin(() => {
+  console.log('Plugin injected by my-module!')
+  return {
+    provide: {
+      myPlugin: {
+        hello: 'foo'
+      }
+    }
+  }
+})
